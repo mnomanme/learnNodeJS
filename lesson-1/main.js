9365695333541e5d0dbc853de4d22ab0650240b6
nomanme <abdullah.noman083@gmail.com>
2022-03-06 00:54:23 +0600

    request response model

--- lesson-1/main.js
+++ lesson-1/main.js
@@ -1,6 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+	if (req.url === '/') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write('<h1>This is Home page</h1>');
+		res.end();
+	} else if (req.url === '/contact') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write('<h1>This is Contact page</h1>');
+		res.end();
+	} else if (req.url === '/about') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write('<h1>This is About page</h1>');
+		res.end();
+	}
+
 	res.end('Hello Node JS World!');
 });
 
