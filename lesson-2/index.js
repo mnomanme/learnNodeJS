56b6e5af308f638c05d59c4b6f4fbd3df25e67b2
nomanme <abdullah.noman083@gmail.com>
2022-03-06 23:55:40 +0600

    fs module file read done

---
+++ lesson-2/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	if (req.url === '/') {
+		// Asynchronous
+		fs.readFile('./index.html', (err, data) => {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(data);
+			res.end();
+		});
+	}
+});
+
+server.listen('5000');
+
+console.log('FS server is running');
