bfe3acf37bd2526b05d923fe6a3065316be59282
nomanme <abdullah.noman083@gmail.com>
2022-03-07 00:13:59 +0600

    file write async

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -3,6 +3,22 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+	// writeFile
+	if (req.url === '/') {
+		fs.writeFile('index.txt', 'Welcome to Node JS', (err, data) => {
+			if (err) {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write('File write Fail');
+				res.end();
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write('File write Success');
+				res.end();
+			}
+		});
+	}
+
+	// readFile
 	if (req.url === '/') {
 		// Asynchronous
 		fs.readFile('./index.html', (err, data) => {
