e0d40d84c3c2a1f76775e0c391fd2783415c940b
nomanme <abdullah.noman083@gmail.com>
2022-03-07 23:12:36 +0600

    file delete async and sync done

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -3,6 +3,34 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+	// deleteFile
+	if (req.url === '/') {
+		// Asynchronous
+		fs.unlink('./delete.txt', (err, data) => {
+			if (err) {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write('File delete Fail');
+				res.end();
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write('File delete Success');
+				res.end();
+			}
+		});
+
+		// Synchronous
+		const deleteFile = fs.unlinkSync('./delete2.txt');
+		if (deleteFile) {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write('File delete Fail');
+			res.end();
+		} else {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write('File delete Success');
+			res.end();
+		}
+	}
+
 	// renameFile
 	if (req.url === '/') {
 		// Asynchronous
@@ -17,24 +45,36 @@ const server = http.createServer((req, res) => {
 				res.end();
 			}
 		});
+
+		// Synchronous
+		const result = fs.renameSync('./homeNew.txt', 'homeNewSync.txt');
+		if (result) {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write('Rename file Fail');
+			res.end();
+		} else {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write('Rename file Success');
+			res.end();
+		}
 	}
 
 	// writeFile
 	if (req.url === '/') {
 		// Asynchronous
-		// fs.writeFile('index.txt', 'Welcome to Node JS', (err, data) => {
-		// 	if (err) {
-		// 		res.writeHead(200, { 'Content-Type': 'text/html' });
-		// 		res.write('File write Fail');
-		// 		res.end();
-		// 	} else {
-		// 		res.writeHead(200, { 'Content-Type': 'text/html' });
-		// 		res.write('File write Success');
-		// 		res.end();
-		// 	}
-		// });
+		fs.writeFile('index.txt', 'Welcome to Node JS', (err, data) => {
+			if (err) {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write('File write Fail');
+				res.end();
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write('File write Success');
+				res.end();
+			}
+		});
+
 		// Synchronous
-		/*
 		const fileError = fs.writeFileSync('demo.txt', 'Welcome to Node JS 2');
 		if (fileError) {
 			res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -45,7 +85,6 @@ const server = http.createServer((req, res) => {
 			res.write('File write Success');
 			res.end();
 		}
-    */
 	}
 
 	// readFile
@@ -58,12 +97,10 @@ const server = http.createServer((req, res) => {
 		});
 
 		// Synchronous
-		/*
 		const myData = fs.readFileSync('./index.html');
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.write(myData);
 		res.end();
-    */
 	}
 });
 
