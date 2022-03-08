d4635dd345088641f592c1d7c4ca76803aeba7b7
nomanme <abdullah.noman083@gmail.com>
2022-03-08 21:32:48 +0600

    insert and delete data

--- lesson-3/server.js
+++ lesson-3/server.js
@@ -5,18 +5,39 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = 'mongodb+srv://merndb-learn:ns81vrzfd4uytmay@cluster0.cy70y.mongodb.net/merndb?retryWrites=true&w=majority';
+const uri = `mongodb+srv://merndb-learn:ns81vrzfd4uytmay@cluster0.cy70y.mongodb.net/merndb?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect((err) => {
+client.connect(() => {
 	const collection = client.db('merndb').collection('dbList');
 
-	if (err) {
-		console.log('DB Connection Failed');
-	} else {
-		console.log('DB Connection Success');
-	}
+	console.log('DB connect successfully');
 
-	client.close();
+	// insertData
+	const dataList = { name: 'Mehe Sultana', age: 20, District: 'Sylhet' };
+
+	collection
+		.insertOne(dataList)
+		.then((result) => {
+			console.log(result);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	console.log('Data insert success');
+
+	// deleteData
+	const deleteData = { age: 20 };
+	collection
+		.deleteOne(deleteData)
+		.then((result) => {
+			console.log(result);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	console.log('Data delete success');
+
+	// client.close();
 });
