1fcb44ad02a6071c663ab1a7342f112a145ee560
nomanme <abdullah.noman083@gmail.com>
2022-03-16 00:05:15 +0600

    post method done

--- lesson-05/index.js
+++ lesson-05/index.js
@@ -15,6 +15,35 @@ app.get('/name', (req, res) => {
 	res.send(firstName + ' ' + lastName);
 });
 
+// Working With Get Request Header
+app.get('/header', (req, res) => {
+	const firstLetter = req.header('firstLetter');
+	const lastLetter = req.header('lastLetter');
+
+	res.send(firstLetter + ' ' + lastLetter);
+});
+
+// Simple Post Request
+app.post('/post', (req, res) => {
+	res.send('This is simple post request');
+});
+
+// Post Request With URL Query
+app.post('/postQuery', (req, res) => {
+	const firstName = req.query.firstName;
+	const lastName = req.query.lastName;
+
+	res.send(firstName + ' ' + lastName);
+});
+
+// Post Request With Header Properties
+app.post('/postHeader', (req, res) => {
+	const username = req.header('username');
+	const password = req.header('password');
+
+	res.send(username + ' ' + password);
+});
+
 app.listen(5000, () => {
 	console.log('Server run successfully on port 5000');
 });
