2800d5f9d1e5f03692bb67f3313053a03e732a77
nomanme <abdullah.noman083@gmail.com>
2022-03-16 23:52:30 +0600

    json multipart fileupload

--- lesson-05/index.js
+++ lesson-05/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 
+const upload = multer();
 const app = express();
+
+app.use(bodyParser.json());
+app.use(upload.array());
+app.use(express.static('public'));
+
+// File Upload
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './my-uploads');
+	},
+	filename: function (req, file, cb) {
+		// const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+		// cb(null, file.originalname + '-' + uniqueSuffix);
+		cb(null, file.originalname);
+	},
+});
+
+const uploadFile = multer({ storage: storage }).single('myfile');
 
 // simple GET request
 app.get('/', (req, res) => {
@@ -42,6 +63,34 @@ app.post('/postHeader', (req, res) => {
 	const password = req.header('password');
 
 	res.send(username + ' ' + password);
+});
+
+// Post application-json
+app.post('/bodyParser', (req, res) => {
+	const data = req.body;
+	const dataString = JSON.stringify(data);
+
+	const jsonName = data['name'];
+
+	res.send(dataString);
+});
+
+// Working With Multipart Form Data
+app.post('/multipart', (req, res) => {
+	const data = req.body;
+
+	res.end(JSON.stringify(data));
+});
+
+// File Upload
+app.post('/file', (req, res) => {
+	uploadFile(req, res, (error) => {
+		if (error) {
+			res.send('File upload failed');
+		} else {
+			res.send('File upload success');
+		}
+	});
 });
 
 app.listen(5000, () => {
